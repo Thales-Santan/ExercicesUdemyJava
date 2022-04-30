eb425e62ce53d677e1339c67dda42cd80b147961
Thales Santana <generationt2h@gmail.com>
2022-04-29 21:51:32 -0300

    Fim do exercicio 1 /Fin d'exercice 1

--- sectioncinqconditionnels/Exercice_un.java
+++ sectioncinqconditionnels/Exercice_un.java
@@ -1,10 +1,27 @@
+/* Ler um número inteiro, e depois dizer se este número é negativo ou não.
+ * Lire un nombre entier, et après dire si ce nombre est négatif ou pas.
+ */
+
 package sectioncinqconditionnels;
+
+import java.util.Scanner;
 
 public class Exercice_un {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
 		
-
+		int n1 = sc.nextInt();
+		if(n1<0) {
+			System.out.println("Le nombre est négatif");
+		} 
+		else if (n1>0) {
+			System.out.println("Le nombre est positif!");
+		}
+		else {
+			System.out.println("Le nombre est neutre!");
+		}
+		sc.close();
 	}
 
 }
