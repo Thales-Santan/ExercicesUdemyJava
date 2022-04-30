2a6429ed588619cc96075ceb5c173c0a70a88380
Thales Santana <generationt2h@gmail.com>
2022-04-29 22:13:42 -0300

    Fim do exercicio 2 / Fin d'exercice 2

--- sectioncinqconditionnels/Exercice_Deux.java
+++ sectioncinqconditionnels/Exercice_Deux.java
@@ -1,10 +1,28 @@
+/*Ler 2 numeros inteiros e informar se sao multiplos ou nao /
+ *Lire 2 nombres entiers et informer s'ils sont multiples ou nom
+ */
+
 package sectioncinqconditionnels;
+
+import java.util.Scanner;
 
 public class Exercice_Deux {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
 		
-
+		int n1 = 0, n2 = 0;
+		n1 = sc.nextInt();
+		n2 = sc.nextInt();
+		
+		if(n1%n2==0) {
+			System.out.println("Les nombres sont multiples!");
+		}
+		else {
+			System.out.println("Les nombres ne sont pas multiples!");
+		}
+		
+		sc.close();
 	}
 
 }
