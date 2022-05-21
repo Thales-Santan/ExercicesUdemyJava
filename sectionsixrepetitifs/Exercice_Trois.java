5a83909f30f9238c40dc25327729be50b6104e48
Thales Santana <generationt2h@gmail.com>
2022-05-21 20:04:16 -0300

    Fim do Ex 3 / Fin de l'exercice 3

--- sectionsixrepetitifs/Exercice_Trois.java
+++ sectionsixrepetitifs/Exercice_Trois.java
@@ -1,10 +1,29 @@
+/*Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o 
+X, se for o caso / Lisez un nombre entier X (1 <= X <= 1000). Ensuite, montrez les impairs de l'un jusqu'au X,
+un par ligne, en incluant le X si necessaire.*/
+
 package sectionsixrepetitifs;
+
+import java.util.Scanner;
 
 public class Exercice_Trois {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int X = 0;
+		
+		while(X<1 || X>1000) {
+			X = sc.nextInt();
+		}
+		
+		for(int i=1 ;i<=X; i++) {
+			if(i%2!=0) {
+				System.out.println(i);
+			}
+		};
+		
+		sc.close();
 	}
 
 }
