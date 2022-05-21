70d80eb5db35c6d122b6506a38ad3b55b02a063c
Thales Santana <generationt2h@gmail.com>
2022-05-21 20:48:04 -0300

    Correcao / Reparation

--- sectionsixrepetitifs/Exercice_Quatre.java
+++ sectionsixrepetitifs/Exercice_Quatre.java
@@ -3,8 +3,8 @@
  * mostrando essas informações conforme exemplo 
  * (use a palavra "in" para dentro do intervalo, e "out" para fora do intervalo)/ 
  * Lisez une valeur entière N. Cette valeur sera la quantité de valeurs entières X qui seront lues ci-dessous.
-Indiquez combien de ces valeurs X se situent dans l'intervalle [10-20] et combien sont hors de l'intervalle, 
-en montrant cette information selon l'exemple (utiliser le mot "in" dans l'intervalle, et "out" à l'extérieur de l'intervalle)*/
+ * Indiquez combien de ces valeurs X se situent dans l'intervalle [10-20] et combien sont hors de l'intervalle, 
+ * en montrant cette information selon l'exemple (utiliser le mot "in" dans l'intervalle, et "out" à l'extérieur de l'intervalle)*/
 
 package sectionsixrepetitifs;
 
