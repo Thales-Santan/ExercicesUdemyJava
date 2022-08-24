a785accb2b0fc3c996424865b74ca0cf919498e5
Thales Santana <generationt2h@gmail.com>
2022-08-24 15:56:07 -0300

    Criando a classe Produto / En creant la classe Produit

--- section7/poo/Produit.java
+++ section7/poo/Produit.java
@@ -1,5 +1,17 @@
 package section7.poo;
 
 public class Produit {
-
+	public String nom;
+	public double valeur;
+	public int quantite;
+	
+	public double valeurTotalDeProduits() {
+		return  valeur * quantite;
+	}
+	
+	public void retireeDesProduits(int quantite) {
+		this.quantite += quantite;
+	}
+	
+	
 }
